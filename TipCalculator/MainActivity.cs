1240c9f4ae9e278fe8abc5250ed1be76b0ebe23f
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2018-05-14 07:32:02 -0700

    * AboutAssets.txt: Initial Push
    * MainActivity.cs:
    * packages.config:
    * TipCalculator.csproj:
    * AssemblyInfo.cs:
    * Main.axml:
    * AboutResources.txt:
    * Strings.xml:
    * AndroidManifest.xml:
    * Icon.png:
    * Icon.png:
    * Resource.designer.cs:
    * Icon.png:
    * Icon.png:
    * Icon.png:

---
+++ TipCalculator/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TipCalculator
+{
+    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
